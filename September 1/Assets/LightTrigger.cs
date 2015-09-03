38b848b4617858356fbf94e76c2a35b8c9600fd3
Ben Norskov <ben@playistheantidote.com>
2015-09-03 17:55:29 -0400

    Thursday's Class

---
+++ September 1/Assets/LightTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// when the player walks into the trigger on this gameobject
+	void OnTriggerEnter() {
+		Debug.Log("The Trigger is working!");
+		// get the light component on this game object, turn it off
+		gameObject.GetComponent<Light>().enabled = false;
+	}
+
+}
---
+++ September 1/Assets/meshTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meshTrigger : MonoBehaviour {
+
+
+	void OnTriggerEnter() {
+		gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+		//find the big cube and turn it off
+		GameObject.Find ("BigBasketCube").SetActive(false);
+
+		//Destroy(GameObject.Find("BigBasketCube") );
+	}
+
+}
