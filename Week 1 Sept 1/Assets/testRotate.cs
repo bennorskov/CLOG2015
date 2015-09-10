613834ed04411bcf882cc1ce0ecd2fac0a0ac142
Ben Norskov <ben@playistheantidote.com>
2015-09-10 09:43:51 -0400

    Added in Week 2's files. Renamed folder structure.

---
+++ Week 1 Sept 1/Assets/testRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<MeshRenderer>().material.color = Color.cyan;
+	
+	}
+}
---
+++ Week2 Sept 8/Assets/PositionChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionChanger : MonoBehaviour {
+	private float xSpeed = .1f;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (gameObject.transform.position);
+
+	}
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = gameObject.transform.position + new Vector3(xSpeed, .01f, 0);
+
+		//if the position gets too far off screen
+		if (gameObject.transform.position.x > 10) {
+			xSpeed = -xSpeed;
+			//reverse direction
+		}
+		//same as above but the other direction. 
+		if (gameObject.transform.position.x < -10) {
+			xSpeed = -xSpeed;
+		}
+	}
+}
