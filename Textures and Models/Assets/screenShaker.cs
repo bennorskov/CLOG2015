4c4bd36d4cfa77f41cf0b50a168dba88f71666be
Ben Norskov <ben@playistheantidote.com>
2015-09-30 11:17:25 -0400

    With Co-Routines!

---
+++ Textures and Models/Assets/screenShaker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenShaker : MonoBehaviour {
+
+	public float shakeAmount = 2f;
+	public float shakeTime = 1.5f;
+
+	private bool isShaking = false;
+	private Vector3 originalLocation;
+
+	void Update () {
+		if ( Input.GetKey(KeyCode.Space) ) {
+			StartCoroutine("shakeScreen");
+		}
+		if (isShaking) {
+			transform.position = originalLocation + Random.insideUnitSphere * Time.deltaTime * shakeAmount;
+		}
+	}
+
+	IEnumerator shakeScreen() {
+		isShaking = true;
+		originalLocation = transform.position;
+		yield return new WaitForSeconds(shakeTime);
+		isShaking = false;
+	}
+}
