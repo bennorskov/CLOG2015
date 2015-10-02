2facf4a2de0e51edc431ada98930373924618811
Ben Norskov <ben@playistheantidote.com>
2015-10-01 21:44:50 -0400

    Sound Files from class on Thursday, October 1st.

---
+++ Week 4:5 Textures and Models/Assets/soundPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundPlayer : MonoBehaviour {
+
+	private AudioSource explode_sound;	
+
+	void Start () {
+		explode_sound = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.G) && explode_sound.isPlaying == false) {
+			explode_sound.Play();
+		}
+	}
+}
