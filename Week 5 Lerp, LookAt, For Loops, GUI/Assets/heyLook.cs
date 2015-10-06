28dc69ae47ab4568ef2b4a275e871986e8e220ba
Ben Norskov <ben@playistheantidote.com>
2015-10-06 18:40:58 -0400

    Lerp and LookAt.

---
+++ Week 5 Lerp, LookAt, For Loops, GUI/Assets/heyLook.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heyLook : MonoBehaviour {
+
+	Transform lerpysPosition;
+
+	// Use this for initialization
+	void Start () {
+		lerpysPosition = GameObject.Find("Lerpy").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(lerpysPosition.position);
+		transform.position += Time.deltaTime * transform.forward;
+	}	
+}
---
+++ Week 5 Lerp, LookAt, For Loops, GUI/Assets/lerpyMover.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lerpyMover : MonoBehaviour {
+	private Transform startPoint, endPoint;
+	public float percentage;
+	public float speed = 2;
+
+	private int direction;
+
+	// Use this for initialization
+	void Start () {
+		startPoint = GameObject.Find("StartPoint").transform;
+		endPoint = GameObject.Find("EndPoint").transform;
+		direction = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = 
+			Vector3.Lerp(startPoint.position, endPoint.position, percentage);
+
+		transform.rotation = Quaternion.Slerp( Quaternion.identity, Quaternion.Euler( new Vector3(-90, 120, 3) ), percentage);
+
+		gameObject.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.cyan, percentage);
+
+		speed = Mathf.Max(speed, .000001f); //don't divide by zero
+
+		percentage += Time.deltaTime/speed * direction;
+
+		//keep percentage within range
+		//switch direction
+		if ((percentage>1) || (percentage < 0)) {
+			direction = -direction;
+			percentage = Mathf.Clamp(percentage, 0, 1);
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
