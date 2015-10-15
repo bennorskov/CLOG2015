5203f60f8fd7380112dd7c7ce97440bf881ae7b7
Ben Norskov <ben@playistheantidote.com>
2015-10-15 11:53:55 -0400

    Raycasting and texture switching!

---
+++ Week 6 Rays/Assets/RayShooter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+	public float rayDistance = 5;
+
+
+	void Start () {
+	}
+	
+	void Update () {
+		RaycastHit hitInfo; // variable to hold information about the raycast
+		//if something is rayDistance away from this object from the front
+		if ( Physics.Raycast( transform.position, transform.forward, out hitInfo, rayDistance) ) {
+			Debug.Log("you've hit the thing named: " + hitInfo.collider.name);
+			hitInfo.collider.GetComponent<packTextures>().changeTexture();
+		}
+	}
+
+	void OnDrawGizmos() {
+		//this is only so we can visualize the ray. Gizmos won't show up in the final game
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(transform.position, transform.forward * rayDistance);
+	}
+}
---
+++ Week 6 Rays/Assets/packTextures.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class packTextures : MonoBehaviour {
+
+	public Texture[] textures;
+	int currentTexture = 0;
+
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+	
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			GetComponent<Renderer>().material.mainTexture = textures[0];
+		}
+	}
+
+	public void changeTexture() {
+		currentTexture += 1;
+		// the "%" is a modulo operator 
+		// it means take the remander after dividing
+		currentTexture = currentTexture % textures.Length; 
+		GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+	}
+}
