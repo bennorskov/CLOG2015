7451e7feace9a0e9e503d5afa036ed1ad1e8a060
Ben Norskov <ben@playistheantidote.com>
2015-10-19 10:31:39 -0400

    Sprite Animations!

---
+++ Week 6 Rays/Assets/Scripts/controllAni.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllAni : MonoBehaviour {
+
+	Animator animationController;
+	void Start () {
+		animationController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow) ) {
+			animationController.SetInteger("Direction", 0);
+		}
+		if (Input.GetKey(KeyCode.DownArrow) ) {
+			animationController.SetInteger("Direction", 2);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) ) {
+			animationController.SetInteger("Direction", 3);
+		}
+		if (Input.GetKey(KeyCode.RightArrow) ) {
+			animationController.SetInteger("Direction", 1);
+		}
+	}
+}
---
+++ Week 6 Rays/Assets/kenMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kenMover : MonoBehaviour {
+
+	Animator kenController;
+	
+	const int ANI_STATE_IDLE = 0;
+	const int ANI_STATE_JUMP = 2;
+	const int ANI_STATE_DUCK = 1;
+
+	void Start () {
+		kenController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow) ){
+			kenController.SetInteger("State", ANI_STATE_JUMP);
+		} else if ( Input.GetKey(KeyCode.DownArrow) ) {
+			kenController.SetInteger("State", ANI_STATE_DUCK);
+		} else {
+			//play idle animation
+			kenController.SetInteger("State", ANI_STATE_IDLE);
+		}
+	}
+}
