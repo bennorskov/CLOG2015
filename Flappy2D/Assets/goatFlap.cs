1fee277513593e5e2da6a9e252582b361e7d1cf8
Ben Norskov <ben@playistheantidote.com>
2015-11-17 17:19:37 -0500

    Little Tweaks

--- Flappy2D/Assets/goatFlap.cs
+++ Flappy2D/Assets/goatFlap.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class goatFlap : MonoBehaviour {
 
-	public float flapForce = 1000f;
+	public float flapForce = 200f;
 
 	private Rigidbody2D rb;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 	}
@@ -14,13 +15,14 @@ public class goatFlap : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Space)) {
 			rb.AddForce( Vector2.up * flapForce );
 		}
+		//rotation
 		float rot = 2 * rb.velocity.y;
 		transform.localRotation = Quaternion.Euler( new Vector3( 0, 0, rot));
 	}
 
 	void OnCollisionEnter2D (Collision2D other) {
 		if (!other.collider.CompareTag("Wall")) {
-			showGameOver();
+			//showGameOver();
 		}
 	}
 	void showGameOver() {
