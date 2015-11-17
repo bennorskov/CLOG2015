3b24bd51080d0b5abd0d27e1489d04d686a2289f
Ben Norskov <ben@playistheantidote.com>
2015-11-17 16:36:39 -0500

    Scrolling Ground!

---
+++ Flappy2D/Assets/scrollGround.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollGround : MonoBehaviour {
+	Material groundMat;
+	private float offset = 0;
+	public float offsetSpeed = 1f;
+	void Start () {
+		groundMat = GetComponent<Renderer>().material;
+	}
+	
+	void Update () {
+		offset += offsetSpeed * Time.deltaTime;
+		groundMat.SetTextureOffset( "_MainTex", new Vector2(offset, 0));
+	}
+}
